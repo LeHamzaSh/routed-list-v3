c06080e6f06a656dc68451874b8d1b1173602361
LeHamzaSh <hrsheikh05@gmail.com>
2021-09-26 01:47:57 +0400

    working tasks and statuses

--- src/components/ViewAllTask.vue
+++ src/components/ViewAllTask.vue
@@ -1,7 +1,49 @@
 <template>
     <div class="home">
-        <h2>i am a ViewAllTask page</h2>
-        <h3>test msg for home page</h3>
+        <div class="container">
+            <h2 class="text-center mt-5">Hamza's Todo List</h2>
+
+            <!-- Table for the Tasks -->
+            <table class="table table-bordered mt-5">
+                <!-- Table Head -->
+                <thead>
+                    <tr>
+                        <th scope="col">Task</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">#</th>
+                        <th scope="col">#</th>
+                    </tr>
+                </thead>
+
+                <!-- Table Body -->
+                <tbody>
+                    <tr v-for="(task, index) in $store.state.tasks" :key="index">
+                    <td><span :class="{'completedItem': task.status === 'Completed'}">{{ task.name }}</span></td>
+                    <!-- Display the name of the task -->
+                    <!-- The Statuses are clickable to change -->
+                    <!-- <td><span>{{ task.name }}</span></td> -->
+                    <td style="width: 140px"><span @click="$store.commit('statusChange', index)" class="pointer" :class="{
+                    'text-danger'  :task.status === 'To-do',
+                    'text-warning' : task.status === 'In-progress',
+                    'text-success'  : task.status === 'Completed'}">
+                    {{ task.status }}
+                    </span></td>
+
+                    <td>
+                        <div class="text-center" @click="$store.commit('editedItem', index)">
+                            <span class="fa fa-pen"></span>
+                        </div>
+                    </td>
+
+                    <td>
+                        <div class="text-center" @click="$store.commit('deleteItem', index)">
+                            <span class="fa fa-trash"></span>
+                        </div>
+                    </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
 
@@ -10,3 +52,39 @@ export default{
     name: 'AddTask',
 }
 </script>
+
+<style scoped>
+.pointer{
+    cursor: pointer;
+}
+
+.completedItem{
+    text-decoration: line-through;
+}
+
+tr{
+    border-color: black;
+
+}
+
+th{
+    font-weight: bold;
+    font-style: italic;
+}
+
+thead{
+    border-color: black;
+    border-width: 5px;
+}
+
+tbody{
+    border-color: black;
+    border-width: 5px;
+}
+
+td{
+    border-color: black;
+    border-width: 5px;
+
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,16 +5,56 @@ export default createStore({
     task: '',
     editedItem: null,
     statusAvailable: ['To-do', 'In-progress', 'Completed'],
-    tasks: [] // An Array where all the tasks would store
+    tasks: [], // An Array where all the tasks would store  
   },
   mutations: {
     submitTasks(state){
       if(state.task.length === 0){
         return alert('It cannot be empty')
       }
+      else{
+        if(state.editedItem === null){
+          console.log(state.task)
+          state.tasks.push({
+            name: state.task,
+            status: 'To-do'
+          })
+        }
+
+        else{
+          state.tasks[state.editedItem].name = state.task;
+          state.editedItem = null
+        }
+
+        // Reset the Enter Task after entering data
+        state.task = ''
+      }
+    },
+
+    deleteItem(state, index){
+      // Remove the item from the array
+      state.tasks.splice(index, 1)
+    },
+
+    editedItem(state,index){
+      // state.task = state.tasks[index].name
+      // console.log(index)
+      // state.editedItem = index
+      // console.log(state.editedItem)
+    },
+
+    statusChange(state, index){
+      // Loop over the statuses on each click
+      let indexTwo = state.statusAvailable.indexOf(state.tasks[index].status)
+      if(++indexTwo > 2) indexTwo = 0
+      state.tasks[index].status = state.statusAvailable[indexTwo]
+      console.log(indexTwo)
+      console.log(state.tasks.status)
     }
+
   },
   actions: {
+
   },
   modules: {
   }
